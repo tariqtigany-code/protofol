324f0bfd0e4ee30be9f3f709305a0171a4530f7e
tariqtigany-code <tariqtigany@gmail.com>
2026-01-04 13:37:46 +0300

    complet exercise -09
    use if condition

---
+++ Hello - 09.py
@@ -0,0 +1,13 @@
+#Define the 'hello' function
+def hello(): 
+    #the function print "Hello"
+    print("Hello")
+
+#Define the 'main' function
+def main():
+    #Call the hello function 
+    hello() 
+
+if __name__ == "__main__":
+    main()       
+
